07af54a35a45889ac65de68edc9214d16b7d376f
eric-lab-star <cyon256@icloud.com>
2022-08-04 13:29:13 +0900

    primitives

---
+++ src/gitJava/Primitve.java
@@ -0,0 +1,24 @@
+package gitJava;
+
+public class Primitve {
+	public static void main(String args[]) {
+		byte b = 1;
+		short s = 2;
+		char c = 'A';
+		
+		int finger = 10;
+		long big = 100_000_000_000L;
+		long hex = 0xFFFF_FFFF_FFFF_FFFFL;
+		
+		int octNumb = 010;
+		int hexNumb = 0x10;
+		int binNumb = 0b10;
+		
+		
+		
+		
+		
+	};
+	
+
+}
